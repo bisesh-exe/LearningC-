0a6b3aedca51d255231b3494bac2c3fc156760f4
bisesh-exe <98681948+bisesh-exe@users.noreply.github.com>
2023-08-17 09:14:25 +0545

    LearningC#.cs

--- LearningC#.cs
+++ LearningC#.cs
@@ -10,7 +10,13 @@ namespace LearningC_
     {
         static void Main(string[] args)
         {
-            
+            String songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+
+            String soonglyrics = "you say goodbye, and i say hello";
+            Console.WriteLine(soonglyrics.StartsWith("you"));
+            Console.WriteLine(soonglyrics.EndsWith("goodbye"));
         }
     }
 }
